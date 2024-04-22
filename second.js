1a4fec675047c6563c6f3c15325e5c7428e25aed
Pavel Koryagin <pavel.koryagin.76@bk.ru>
2024-04-22 21:54:06 +0800

    duble game

--- second.js
+++ second.js
@@ -1,1 +1,50 @@
 'use strict';
+function gameBot() {
+  let sicretNumber = Math.floor(Math.random() * 100) + 1;
+  console.log(sicretNumber);
+  let attempts = 5;
+
+    function playGame(){
+      let userGuess = prompt(`Угадай число от 1 до 100 . Осталось ${attempts} попыток`);
+    
+    if (userGuess === null) {
+      alert('Игра окончена');
+      return;
+    }
+
+    userGuess = Number(userGuess);
+
+    if (isNaN(userGuess)) {
+      alert('Введи число!');
+    } else if (userGuess > sicretNumber) {
+      alert('Загаданное число меньше');
+    } else if (userGuess < sicretNumber) {
+      alert('Загаданное число больше');
+    } else {
+      if (confirm("Поздравляю, Вы угадали!!! Хотите сыграть еще?")) {
+        gameBot();
+        return;
+      } else {
+        alert("Игра окончена.");
+        return;
+      }
+    }
+
+   
+    attempts--;
+
+    if (attempts === 0) {
+      if(confirm("Попытки закончились, хотите сыграть еще?")) {
+        gameBot();
+        return;
+      } else {
+        alert('"Игра окончена."');
+        return;
+      }
+    }
+    playGame();
+}
+playGame();
+}
+
+gameBot();
