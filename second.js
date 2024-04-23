dfe0c05dfe36cea37d68109b20c5091af238f21b
Pavel Koryagin <pavel.koryagin.76@bk.ru>
2024-04-23 13:22:51 +0800

    repeated the code

--- second.js
+++ second.js
@@ -1,50 +1,52 @@
 'use strict';
-function gameBot() {
-  let sicretNumber = Math.floor(Math.random() * 100) + 1;
-  console.log(sicretNumber);
-  let attempts = 5;
 
-    function playGame(){
-      let userGuess = prompt(`Угадай число от 1 до 100 . Осталось ${attempts} попыток`);
-    
-    if (userGuess === null) {
-      alert('Игра окончена');
+function gameBot () {
+   let sicretNumber = Math.floor(Math.random() * 100) + 1;
+   console.log(sicretNumber);
+
+   let attempts = 7;
+
+   function playGame() {
+
+    let userGuess = prompt(`Угадай число от 1 до 100.Осталось ${attempts}  попыток:`);
+
+    if(userGuess === null) {
+      alert('Game over!');
       return;
     }
 
     userGuess = Number(userGuess);
 
-    if (isNaN(userGuess)) {
-      alert('Введи число!');
-    } else if (userGuess > sicretNumber) {
-      alert('Загаданное число меньше');
-    } else if (userGuess < sicretNumber) {
-      alert('Загаданное число больше');
+    if(isNaN(userGuess)) {
+      alert("Введи число!");
     } else {
-      if (confirm("Поздравляю, Вы угадали!!! Хотите сыграть еще?")) {
-        gameBot();
-        return;
+      if (userGuess > sicretNumber) {
+        alert("Загаданное число меньше.");
+      } else if (userGuess < sicretNumber) {
+        alert("Загаданное число больше.");
       } else {
-        alert("Игра окончена.");
-        return;
+        if (confirm("Поздравляю, Вы угадали!!! Хотите сыграть еще?")) {
+          gameBot();
+          return;
+        } else {
+          alert('Game over!!!');
+          return;
+        }
       }
-    }
+      attempts--;
 
-   
-    attempts--;
-
-    if (attempts === 0) {
-      if(confirm("Попытки закончились, хотите сыграть еще?")) {
-        gameBot();
-        return;
-      } else {
-        alert('"Игра окончена."');
-        return;
+      if (attempts === 0) {
+        if (confirm("Попытки закончились, хотите сыграть еще?")) {
+          gameBot();
+          return;
+        } else {
+          alert('Game over');
+          return;
+        }
       }
+      playGame();
     }
-    playGame();
+   }
+   playGame();
 }
-playGame();
-}
-
 gameBot();
